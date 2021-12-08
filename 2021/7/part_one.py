c7c7c8273c38c27576f76f7e7c90817e13c23bba
griever255 <jgargiulo95@gmail.com>
2021-12-07 19:10:31 -0500

    Finished Day 7

---
+++ 2021/7/part_one.py
@@ -0,0 +1,20 @@
+### Part One
+
+filename = "input.txt"
+
+with open(filename, "r") as f:
+    positions = f.read().split(",")
+    positions = list(map(int, positions))
+f.close()
+
+min = min(positions)
+max = max(positions)
+
+best_fuel = 99999999
+for middle in range(min, max):
+    fuel = 0
+    for position in positions:
+        fuel += abs(position-middle)
+    if fuel < best_fuel:
+        best_fuel = fuel
+print(best_fuel)
